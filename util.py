6a9eb636667fc20ff2de0164c544235393b98de4
Joona Yoon <joonas.yoon@gmail.com>
2020-04-05 21:43:56 +0900

    run selenium as headless mode with argument

--- util.py
+++ util.py
@@ -1,7 +1,7 @@
 import os, bs4
 from selenium import webdriver
 
-APP_DIR = os.path.dirname(os.path.dirname("__filename__"))
+APP_DIR = os.path.dirname(os.path.dirname('__filename__'))
 
 
 def get_text(element):
@@ -17,18 +17,18 @@ class Chrome:
     _chrome_options = webdriver.ChromeOptions()
     _driver = None
 
-    def __init__(self):
-        # Uncomment if you DO NOT want GUI
-        # self._chrome_options.add_argument('--headless')
-        # self._chrome_options.add_argument('--no-sandbox')
-        # self._chrome_options.add_argument('--disable-dev-shm-usage')
-        # self._chrome_options.add_argument('--disable-gpu')
+    def __init__(self, headless=False):
+        if headless:
+            self._chrome_options.add_argument('--headless')
+            self._chrome_options.add_argument('--no-sandbox')
+            self._chrome_options.add_argument('--disable-dev-shm-usage')
+            self._chrome_options.add_argument('--disable-gpu')
         self._chrome_options.add_argument('--window-size=1920x1080')
         self._chrome_options.add_argument('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36')
         self._driver = self.create_driver()
 
     def create_driver(self):
-        driver = webdriver.Chrome(self._chrome_driver, chrome_options=self._chrome_options)
+        driver = webdriver.Chrome(self._chrome_driver, options=self._chrome_options)
         return driver
 
     def get_driver(self):
